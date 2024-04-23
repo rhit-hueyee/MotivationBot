c6e9fcf61fa16ee7045d4130c0c8bc7dbed7827f
ehuey275 <hueyee@rose-hulman.edu>
2024-04-23 01:43:30 -0400

    CI Work
    Libraries Setup
    ENV Setup
    Tests Setup

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch
+from main import get_user_id
+
+class TestBotFunctions(unittest.TestCase):
+    @patch('builtins.open')
+    @patch('csv.DictReader')
+    def test_get_user_id(self, mock_dict_reader, mock_open):
+        mock_dict_reader.return_value = iter([{'username': 'applause7', 'id': '12345'}])
+        user_id = get_user_id('applause7')
+        self.assertEqual(user_id, '12345')
+
+if __name__ == '__main__':
+    unittest.main()
