6ec5bb917dc46dfe01b1c894977fcedc2bc324ec
ehuey275 <hueyee@rose-hulman.edu>
2024-06-05 22:03:45 -0700

    Multiple Messages and Random Messages

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+# src/__init__.py
+
+# Optionally import some common utilities or needed classes/functions
+from .utils import get_random_message_by_username, get_user_id  # noqa: F401
--- src/tasks.py
+++ src/tasks.py
@@ -1,13 +1,13 @@
 import random
 from discord.ext import tasks
-from utils import get_user_id
+from . import get_user_id, get_random_message_by_username
 
 
 @tasks.loop(minutes=random.uniform(1, 15))
 async def dm_random_message(client):
     user = await client.fetch_user(get_user_id('applause7'))
     print("User Acquired.")
-    messages = ["Message"]
+    message = get_random_message_by_username('applause7')
     if user:
-        await user.send(random.choice(messages))
+        await user.send(message)
         print("Message Sent.")
